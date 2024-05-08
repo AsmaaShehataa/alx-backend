030982e6b8964c03bca6dc6f76e8236e623d55b6
AsmaaShehataa <asmaa_shihata@yahoo.com>
2024-05-08 10:38:40 +0300

    feat: Update LIFOCache class implementation
    This commit updates the implementation of the `LIFOCache` class in the `1-fifo_cache.py` file. The changes include modifying the `put` method to handle cache eviction based on the LIFO (Last-In, First-Out) strategy. The `key_indexes` list is used to keep track of the order of item insertion, and when the cache is full, the oldest item is discarded.
    
    The purpose of this commit is to improve the cache eviction system by implementing the LIFO strategy, allowing for more efficient management of cached items based on their insertion order.

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,30 +1,46 @@
-#!/usr/bin/python3
-""" FIFO caching
+#!/usr/bin/env python3
 """
+LIFO Caching
+"""
+
 
 BaseCaching = __import__('base_caching').BaseCaching
 
 
-class Fifo(BaseCaching):
-    """ FIFO cache eviction system """
+class LIFOCache(BaseCaching):
+    """
+    class LIFOCache that inherits from BaseCaching and is a caching system
+    """
+
     def __init__(self):
-        """ Constructor """
+        """
+        init method
+        """
         super().__init__()
-        self.queue = []
+        self.key_indexes = []
 
     def put(self, key, item):
-        """ Add an item in the cache """
+        """
+        Must assign to the dictionary self.cache_data
+        the item value for the key
+        """
         if key and item:
-            if key in self.cache_data:
-                self.cache_data[key] = item
-                return
-            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-                discard = self.queue.pop(0)
-                del self.cache_data[discard]
-                print("DISCARD: {}".format(discard))
+            if len(self.cache_data) >= self.MAX_ITEMS:
+                if key in self.cache_data:
+                    del self.cache_data[key]
+                    self.key_indexes.remove(key)
+                else:
+                    del self.cache_data[self.key_indexes[self.MAX_ITEMS - 1]]
+                    item_discarded = self.key_indexes.pop(self.MAX_ITEMS - 1)
+                    print("DISCARD:", item_discarded)
+
             self.cache_data[key] = item
-            self.queue.append(key)
+            self.key_indexes.append(key)
 
     def get(self, key):
-        """ Get an item by key """
-        return self.cache_data.get(key, None)
+        """
+        Must return the value in self.cache_data linked to key.
+        """
+        if key in self.cache_data:
+            return self.cache_data[key]
+        return None
