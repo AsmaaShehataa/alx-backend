8e6b054d0f0e79a13d9ae3f2fa95c72d7d0aa89a
AsmaaShehataa <asmaa_shihata@yahoo.com>
2024-05-15 01:59:38 +0300

    refactor: Update Flask app route to use hello_world function
    This commit updates the Flask app route to use the `hello_world` function instead of the `index` function. The purpose of this change is to improve the clarity and consistency of the codebase by using a more descriptive function name for the route handler. This change does not affect the functionality of the application.

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,17 +1,13 @@
 #!/usr/bin/env python3
-"""
-Get locale from request
+"""A simple flask application
 """
 
-import babel
+
 from flask import Flask, render_template, request
 from flask_babel import Babel
 
-app = Flask(__name__)
-babel = Babel(app)
 
-
-class Config:
+class Config(object):
     """
     Config class
     """
@@ -20,24 +16,27 @@ class Config:
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
+# configure the flask app
+app = Flask(__name__)
 app.config.from_object(Config)
+babel = Babel(app)
 
 
 @babel.localeselector
 def get_locale():
     """
-     determine the best match with supported languages.
+    get locale method
     """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-@app.route('/', methods=['GET'], strict_slashes=False)
+@app.route('/')
 def index():
     """
-    hello world
+    index
     """
-    return render_template('1-index.html')
+    return render_template('2-index.html')
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(port="5000", host="0.0.0.0", debug=True)
